7566106c022d158b374446f782d4b324c60e81b0
jb838 <jacob.blasek1220@gmail.com>
2018-10-15 11:05:40 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner kbReader
+	char a = 'a';
+
+    int aint = ((int)a) + 5;
+
+
+        System.out.println((char)aint);
+
+        for ()
+    }
+}
