210bcef408873b90020a02bbfc529d8f9ed7d390
jblasek <jacob.blasek1220@gmail.com>
2018-10-18 21:47:46 -0400

    classes are dumb why cant i just do it in main :(

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner kbReader
-	char a = 'a';
+        Scanner kbInput = new Scanner(System.in);
+        String wordInput;
+        int numInput;
+        Store input = new Store();
 
-    int aint = ((int)a) + 5;
+        System.out.println("Press 1 to encrypt, or press 2 to decrypt: ");
+
+        numInput = kbInput.nextInt();
+
+        if(numInput == 1){
+            System.out.println("Enter the word you want to encrypt: ");
+            wordInput = kbInput.next();
+            System.out.println("The word encrypted is: " + input.encrypt(wordInput));
+        }
+        else if (numInput == 2){
+            System.out.println("Enter the word you want to decrypt: ");
+            wordInput = kbInput.next();
+            System.out.println("The decryted word is: " + input.decrypt(wordInput));
+        }
 
 
-        System.out.println((char)aint);
-
-        for ()
     }
 }
